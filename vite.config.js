75ee58e1315f95b1b66874a1873cd4a01eefd960
Dayeon-T <dayeont001@e-mirim.hs.kr>
2025-11-28 08:05:57 +0900

    Update base url for LSY repo

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import svgr from "@svgr/rollup";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
-  base: "/website_portfolio/",
+  base: "/LSY/",
   server: {
     proxy: {
       "/api/velog/rss": {
